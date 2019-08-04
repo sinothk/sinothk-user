f17f7c50144a8b25014d306eb47d8de70fb1c4d2
LiangYT <381518188@qq.com>
2019-08-04 15:53:36 +0800

    初始化

--- src/main/java/com/sinothk/user/controller/UserController.java
+++ src/main/java/com/sinothk/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.sinothk.user.controller;
 
 import com.sinothk.base.entity.ResultData;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +12,18 @@ public class UserController {
 
     @GetMapping("/add")
 //    @Transactional
-    public ResultData<String> userRegister() {
+    public ResultData<String> userRegister(@RequestHeader("token") String token, @RequestHeader("token2") String token2) {
         // http://localhost:11000/user/add
         //
 
+        System.out.println("token = " + token);
+        System.out.println("token2 = " + token2);
 
 //        UserEntity userEntity = new UserEntity();
 //        userEntity.setUserName("liangyt");
 ////        userEntity.setUserPwd(password);
 //        return roleService.addUser(userEntity);
+
 
         return ResultData.error("产出Id = ");
     }
